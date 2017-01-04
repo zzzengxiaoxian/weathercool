9042fba9847316fc5b3cc8de91620bc9c2338895
liudongming <928012536@qq.com>
2017-01-04 10:07:20 +0800

    添加数据类和各项配置

---
+++ app/src/main/java/com/weathercool/ltkj/weathercool/db/City.java
@@ -0,0 +1,47 @@
+package com.weathercool.ltkj.weathercool.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dell on 2017/1/4.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;//市名字
+    private int cityCode;//市的代号
+    private int provinceId;//当前市所属的省的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
